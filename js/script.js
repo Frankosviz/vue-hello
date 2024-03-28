b51ae63af8b9e0963b70fcef6f986f224ab3a9a1
Frankosviz <geom.francescopaolo@gmail.com>
2024-03-28 02:39:30 +0100

    Hello Vue - Finished

--- js/script.js
+++ js/script.js
@@ -3,11 +3,21 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!',
-            image: 'img-vue.jpg',
-        },
-            methods: {
-
+            message: 'Hello Vue! I\m Francesco and i worked until 1:00!. I\m still learning Vue!, even if i\'m tired... If the background color will be red i will show an image under this message...',
+            textMessage: 'Here\s your image! Good Night!',
+            btnMessage: 'Try me',
+            myClass: 'blue',
+            myClassRed: 'red',
+            image: '../img/img-vue.jpg',
+        }
+    },
+    methods: {
+        changeColor() {
+            if(this.myClass === 'blue'){
+                this.myClass = 'red';
+            } else {
+                this.myclass = 'blue';
+            }
         }
     }
-})
+}).mount('#app');
